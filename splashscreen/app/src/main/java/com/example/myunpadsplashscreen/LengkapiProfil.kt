db45e685b5a1553de333e94c43ef18de20378990
anshafarell <anshafarell@gmail.com>
2023-05-08 13:21:12 +0700

    Pengaturan - Lengkapi Profil - Scroll Testing

---
+++ splashscreen/app/src/main/java/com/example/myunpadsplashscreen/LengkapiProfil.kt
@@ -0,0 +1,11 @@
+package com.example.myunpadsplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LengkapiProfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lengkapi_profil)
+    }
+}
---
+++ splashscreen/app/src/main/java/com/example/myunpadsplashscreen/ScrollTest.kt
@@ -0,0 +1,11 @@
+package com.example.myunpadsplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_test)
+    }
+}
