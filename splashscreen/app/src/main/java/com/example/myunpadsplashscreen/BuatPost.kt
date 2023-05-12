08fba0df2f7e8f807731cf721162f6d389523d35
anshafarell <anshafarell@gmail.com>
2023-05-12 20:05:31 +0700

    added buat post

---
+++ splashscreen/app/src/main/java/com/example/myunpadsplashscreen/BuatPost.kt
@@ -0,0 +1,11 @@
+package com.example.myunpadsplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatPost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_post)
+    }
+}
