377eba100e32e74d0f362acecc4bd525541c8176
anshafarell <anshafarell@gmail.com>
2023-05-22 20:56:28 +0700

    added ProfilOrang and changes in AndroidManifest

--- splashscreen/app/src/main/java/com/example/myunpadsplashscreen/ProfilOrang.kt
+++ splashscreen/app/src/main/java/com/example/myunpadsplashscreen/ProfilOrang.kt
@@ -8,9 +8,9 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AlertDialog
 
-class ShowPost : ComponentActivity() {
+class ProfilOrang : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_post)
+        setContentView(R.layout.activity_profil_orang)
     }
 }
